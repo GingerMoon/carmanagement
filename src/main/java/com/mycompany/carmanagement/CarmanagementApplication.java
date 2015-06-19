ef6422b1c159a41ab83ebb7da7a368b1a296a8ef
Luffy <376752150@qq.com>
2015-06-19 08:55:02 +0800

    Spring Boot Deployment
    ./gradlew build && java -jar build/libs/carmanagement.war

--- src/main/java/com/mycompany/carmanagement/CarmanagementApplication.java
+++ src/main/java/com/mycompany/carmanagement/CarmanagementApplication.java
@@ -3,11 +3,19 @@ package com.mycompany.carmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-public class CarmanagementApplication {
+public class CarmanagementApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CarmanagementApplication.class);
+	}
+	
 	public static void main(String[] args) {
         SpringApplication.run(CarmanagementApplication.class, args);
     }
