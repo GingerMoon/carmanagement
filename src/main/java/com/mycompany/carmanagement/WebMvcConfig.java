8ed5a087ff166e9651d35647e9fda1275c27f393
Luffy <376752150@qq.com>
2015-06-19 08:55:44 +0800

    Add resource path slimtable
    addResourceHandlers

--- src/main/java/com/mycompany/carmanagement/WebMvcConfig.java
+++ src/main/java/com/mycompany/carmanagement/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.DateFormatter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -13,6 +14,11 @@ import com.mycompany.carmanagement.interceptor.ThymeleafLayoutInterceptor;
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/slimtable/**").addResourceLocations("classpath:/static/slimtable/");
+    }
+	
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
