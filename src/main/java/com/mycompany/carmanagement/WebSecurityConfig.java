45a57922b924dc1816b954729f9f2f0a99c05c4f
Luffy <376752150@qq.com>
2015-06-19 08:53:45 +0800

    Cannot execute JQuery, <script><\script>.
    Response header
    
    x-content-type-options nosniff

--- src/main/java/com/mycompany/carmanagement/WebSecurityConfig.java
+++ src/main/java/com/mycompany/carmanagement/WebSecurityConfig.java
@@ -13,6 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+        	.headers().disable()
             .authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
                 .anyRequest().authenticated()
