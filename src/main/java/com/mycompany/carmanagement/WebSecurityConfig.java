b71c24329337a70b9a4b0b8efc6039491d685b55
Luffy <376752150@qq.com>
2015-06-21 06:29:23 +0800

    WebSecurityConfig .headers().disable() 17. Security HTTP Response Headers X-Content-Type-Options: nosniff http://docs.spring.io/spring-security/site/docs/current/reference/htmlsingle/#headers

--- src/main/java/com/mycompany/carmanagement/WebSecurityConfig.java
+++ src/main/java/com/mycompany/carmanagement/WebSecurityConfig.java
@@ -13,7 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-        	.headers().disable()
+        	//.headers().disable()
             .authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
                 .anyRequest().authenticated()
