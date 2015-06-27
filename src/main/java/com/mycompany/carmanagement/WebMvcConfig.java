87944db9fa2389c9514f1f1d7cd370fd9d340a7e
Luffy <376752150@qq.com>
2015-06-27 17:55:30 +0800

    1. Request URL:http://localhost:8080/purchaseRecord/getAll?jtStartIndex=0&jtPageSize=10 Request Method:POST Form Data id=-1&car.id=-1&employee.id=-1&provider.id=-1&beginDate=2015-01-01&endDate=2015-12-31
    2.
    public void addFormatters(FormatterRegistry registry)
    StringToEntityConverter
    HTTP string parameter (for example, carId:-1) to entity (get from database, return null if not found)

--- src/main/java/com/mycompany/carmanagement/WebMvcConfig.java
+++ src/main/java/com/mycompany/carmanagement/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.mycompany.carmanagement;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.DateFormatter;
@@ -9,6 +10,9 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.mycompany.carmanagement.domain.Car;
+import com.mycompany.carmanagement.domain.Employee;
+import com.mycompany.carmanagement.domain.Provider;
 import com.mycompany.carmanagement.interceptor.ThymeleafLayoutInterceptor;
 
 @Configuration
@@ -29,10 +33,25 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     protected void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new ThymeleafLayoutInterceptor());
     }
-    
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
+//    	registry.addConverter(carConverter());
+//    	registry.addConverter(employeeConverter());
+//    	registry.addConverter(providerConverter());
         registry.addFormatter(new DateFormatter("yyyy-MM-dd"));
     }
 
+    @Bean
+    public StringToEntityConverter carConverter() {
+        return new StringToEntityConverter(Car.class);
+    }
+    @Bean
+    public StringToEntityConverter employeeConverter() {
+        return new StringToEntityConverter(Employee.class);
+    }
+    @Bean
+    public StringToEntityConverter providerConverter() {
+        return new StringToEntityConverter(Provider.class);
+    }
 }
